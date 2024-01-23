c0721c7c54c268879dc60e6121772e4b37353d74
adjfangjiawei <3195727347@qq.com>
2024-01-23 18:19:42 +0800

    Update aptinstall.sh

--- install/aptinstall.sh
+++ install/aptinstall.sh
@@ -31,3 +31,4 @@ sudo apt-get install python3-sphinx
 sudo apt-get install libncurses5-dev
 sudo apt-get install libncurses5-dev libncursesw5-dev
 apt-get install libxaw7-dev
+sudo apt install libibus-1.0-dev
